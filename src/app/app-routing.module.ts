48ece89b419e1accee8e21c5839d3933f1313972
MeidtK <meidtk@ferris.edu>
2019-12-10 21:33:36 -0500

    fuck i forgot i was doing the git thing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EditorComponent } from './editor/editor.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 const routes: Routes = [
  { path: 'editor', component: EditorComponent},
- {path: '', redirectTo: 'editor', pathMatch: 'full'}
+ { path: 'details', component: ProductDetailsComponent},
+ { path: 'about', component: AboutUsComponent},
+ {path: '', redirectTo: 'details', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditorComponent } from './editor/editor.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditorComponent
+    EditorComponent,
+    ProductDetailsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
